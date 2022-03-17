f745317c148bd2479b13b40ca7471933c8b81bb2
SafonovO <olegsafonov01@gmail.com>
2022-03-17 20:25:26 +0200

    Created the Category controller and the Category View

---
+++ UdemyProject/Controllers/CategoryController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UdemyProject.Data;
+using UdemyProject.Models;
+
+namespace UdemyProject.Controllers
+{
+    public class CategoryController : Controller
+    {
+
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+        _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Category> categorylist = _db.Categories;
+            return View(categorylist);
+        }
+    }
+}
