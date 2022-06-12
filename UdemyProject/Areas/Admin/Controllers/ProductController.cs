ec0d6a3e2c6df774383f63f6fb9cbcd52e241a2a
SafonovO <olegsafonov01@gmail.com>
2022-06-12 19:40:47 +0200

    Sdelal mnogo prikolov

--- UdemyProject/Areas/Admin/Controllers/ProductController.cs
+++ UdemyProject/Areas/Admin/Controllers/ProductController.cs
@@ -5,95 +5,118 @@ using UdemyProject.DataAccess.Repository.IRepository;
 using UdemyProject.Models;
 using UdemyProject.Models.ViewModels;
 
-namespace UdemyProject.Controllers
+namespace UdemyProject.Controllers;
+
+public class ProductController : Controller
 {
-    public class ProductController : Controller
+
+    private readonly IUnitOfWork _unitOfWork;
+    private readonly IWebHostEnvironment _hostEnvironment;
+
+    public ProductController(IUnitOfWork unitOfWork,IWebHostEnvironment hostEnvironment)
     {
-
-        private readonly IUnitOfWork _unitOfWork;
-
-        public ProductController(IUnitOfWork unitOfWork)
-        {
-        _unitOfWork = unitOfWork;
-        }
-
-        public IActionResult Index()
-        {
-            IEnumerable<Product> deviceclasslist = _unitOfWork.Product.GetAll();
-            return View(deviceclasslist);
-        }
-
-   
-        public IActionResult Upsert(int? id)
-        {
-            ProductVM productVM = new()
-            {
-                Product = new(),
-                CategoryList = _unitOfWork.Category.GetAll().Select(i => new SelectListItem
-                {
-                    Text = i.Name,
-                    Value = i.Id.ToString()
-                }),
-                DeviceClassList = _unitOfWork.DeviceClass.GetAll().Select(i => new SelectListItem
-                {
-                    Text = i.Name,
-                    Value = i.Id.ToString()
-                }),
-            };
-
-            if (id is null || id ==0)
-            {
-                
-                return View(productVM); 
-            }
-            else
-            {
-                
-            }
-
-
-            return View(productVM);
-        }
-
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public IActionResult Upsert(DeviceClass obj)
-        {
-            if (ModelState.IsValid)
-            {
-                _unitOfWork.DeviceClass.Update(obj);
-                _unitOfWork.Save();
-                TempData["success"] = "Device class updated successfully!";
-                return RedirectToAction("Index");
-            }
-            return View(obj);
-        }
-
-        public IActionResult Delete(int? id)
-        {
-            if (id is null || id == 0)
-            { return NotFound(); }
-
-            var categoryfromdb = _unitOfWork.DeviceClass.GetFirstOrDefault(u => u.Id == id);
-            if (categoryfromdb is null)
-                return NotFound();
-
-
-            return View(categoryfromdb);
-        }
-
-        [HttpPost,ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public IActionResult DeletePOST(int? id)
-        {
-            var obj = _unitOfWork.DeviceClass.GetFirstOrDefault(u => u.Id == id);
-            if (obj is null) { return NotFound(); }
-            _unitOfWork.DeviceClass.Remove(obj);
-            _unitOfWork.Save();
-            TempData["success"] = "Device class deleted successfully!";
-                return RedirectToAction("Index");
-          
-        }
-
+    _unitOfWork = unitOfWork;
+        _hostEnvironment = hostEnvironment;
     }
+
+    public IActionResult Index()
+    {
+       
+        return View();
+    }
+
+
+    public IActionResult Upsert(int? id)
+    {
+        ProductVM productVM = new()
+        {
+            Product = new(),
+            CategoryList = _unitOfWork.Category.GetAll().Select(i => new SelectListItem
+            {
+                Text = i.Name,
+                Value = i.Id.ToString()
+            }),
+            DeviceClassList = _unitOfWork.DeviceClass.GetAll().Select(i => new SelectListItem
+            {
+                Text = i.Name,
+                Value = i.Id.ToString()
+            }),
+        };
+
+        if (id is null || id ==0)
+        {
+            
+            return View(productVM); 
+        }
+        else
+        {
+            
+        }
+
+
+        return View(productVM);
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult Upsert(ProductVM obj, IFormFile? file)
+    {
+        if (ModelState.IsValid)
+        {
+            string wwRootPath = _hostEnvironment.WebRootPath;
+            if (file != null)
+            {
+                string fileName = Guid.NewGuid().ToString() + "_" + file.FileName;
+                var uploads = Path.Combine(wwRootPath, @"Images\Products");
+                var extension = Path.GetExtension(file.FileName);
+
+                using (var filestreams = new FileStream(Path.Combine(uploads, fileName + extension), FileMode.Create))
+                {
+                    file.CopyTo(filestreams);
+                }
+                obj.Product.ImageUrl = @"\Images\Products\" + fileName + extension;
+            }
+            _unitOfWork.Product.Add(obj.Product);
+            _unitOfWork.Save();
+            TempData["success"] = "Product created successfully!";
+            return RedirectToAction("Index");
+        }
+        return View(obj);
+    }
+
+    public IActionResult Delete(int? id)
+    {
+        if (id is null || id == 0)
+        { return NotFound(); }
+
+        var categoryfromdb = _unitOfWork.DeviceClass.GetFirstOrDefault(u => u.Id == id);
+        if (categoryfromdb is null)
+            return NotFound();
+
+
+        return View(categoryfromdb);
+    }
+
+    [HttpPost,ActionName("Delete")]
+    [ValidateAntiForgeryToken]
+    public IActionResult DeletePOST(int? id)
+    {
+        var obj = _unitOfWork.DeviceClass.GetFirstOrDefault(u => u.Id == id);
+        if (obj is null) { return NotFound(); }
+        _unitOfWork.DeviceClass.Remove(obj);
+        _unitOfWork.Save();
+        TempData["success"] = "Device class deleted successfully!";
+            return RedirectToAction("Index");
+      
+    }
+    #region API CALLS
+    [HttpGet]
+
+    public IActionResult getAll()
+    {
+        var productlist = _unitOfWork.Product.GetAll();
+        return Json(new { data = productlist });
+    }
+    #endregion
 }
+
---
+++ UdemyProject/wwwroot/js/product.js
@@ -0,0 +1,20 @@
+﻿var dataTable;
+
+$(document).ready(function () {
+    loadDataTable();
+});
+
+function loadDataTable(){
+    dataTable = $('#tblData').DataTable({
+        "ajax": {
+            "url":"/Admin/Product/getAll"
+        },
+        "columns": [
+            { "data": "Name", "width": "15%" },
+            { "data": "Description", "width": "15%" },
+            { "data": "Manufacturer", "width": "15%" },
+            { "data": "Category", "width": "15%" },
+            { "data": "Price", "width": "15%" },
+        ]
+    });
+}
