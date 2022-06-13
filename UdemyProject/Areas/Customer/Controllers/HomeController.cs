b97539a9f9366b8c2240ab453d91fe6fc66d999b
SafonovO <olegsafonov01@gmail.com>
2022-06-14 01:27:33 +0200

    Added and designed the Home page

--- UdemyProject/Areas/Customer/Controllers/HomeController.cs
+++ UdemyProject/Areas/Customer/Controllers/HomeController.cs
@@ -1,21 +1,39 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using UdemyProject.DataAccess.Repository.IRepository;
 using UdemyProject.Models;
+using UdemyProject.Models.ViewModels;
 
 namespace UdemyProject.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IUnitOfWork _unitOfWork;
 
-        public HomeController(ILogger<HomeController> logger)
+
+        public HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork)
         {
             _logger = logger;
+            _unitOfWork = unitOfWork;
         }
 
         public IActionResult Index()
         {
-            return View();
+
+            IEnumerable<Product> productList = _unitOfWork.Product.GetAll(includeProperties:"Category,DeviceClass");
+            return View(productList);
+        }
+
+        public IActionResult Details(int id)
+        {
+
+            ShoppingCart cartObj = new()
+            {
+                Count=1,
+                Product =  _unitOfWork.Product.GetFirstOrDefault(u => u.ID == id, includeProperties: "Category,DeviceClass"), 
+            };
+            return View(cartObj);
         }
 
         public IActionResult Privacy()
